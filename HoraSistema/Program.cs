95ef6e1a4b1762bbb195079e2d52b53579e07912
MWLeonel <wmleonel6@gmail.com>
2020-09-25 00:09:43 -0300

    Exibir Hora Atual do Sistema

---
+++ HoraSistema/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HoraSistema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime horaAtual;
+
+            horaAtual = DateTime.Now;
+            Console.Write("Data e Hora Atual: ");
+            Console.WriteLine($"{horaAtual}");
+            Console.WriteLine();
+
+            Console.WriteLine("Pressione ENTER para sair!");
+            Console.ReadKey();
+        }
+    }
+}
